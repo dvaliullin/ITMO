c002c0d91a647f4b68d358c4a901b795bbb3cc36
dvaliullin <dvaliullin266@gmail.com>
2026-02-13 18:16:38 +0300

    Update FocusBlast.java

--- Программирование/Lab2/src/moves/FocusBlast.java
+++ Программирование/Lab2/src/moves/FocusBlast.java
@@ -1,4 +1,4 @@
-package move;
+package moves;
 import ru.ifmo.se.pokemon.*;
 
 
@@ -18,3 +18,4 @@ public class FocusBlast extends SpecialMove {
         return "применяет специальную боевую атаку";
     }
 }
+

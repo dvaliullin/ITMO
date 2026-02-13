9bd48582096924a72d17994178d50cac057f4f83
dvaliullin <dvaliullin266@gmail.com>
2026-02-13 19:25:30 +0300

    Add files via upload

---
+++ Программирование/Lab4/src/places/Island.java
@@ -0,0 +1,72 @@
+package places;
+import exceptions.*;
+import events.*;
+import characters.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Island extends Place {
+    public ArrayList<IslandVisitor> visitors = new ArrayList<>();
+    public ArrayList<Event> events = new ArrayList<>();
+
+    public Island(String name) throws NameException {
+        super(name);
+    }
+
+    @Override
+    public void startEvents(LocalDate startDate, int eventsDuration) {
+        for (int days = 1; days < eventsDuration; days++) {
+            LocalDate currentDate = startDate.plusDays(days);
+            for (IslandVisitor visitor : visitors) {
+                for (IslandVisitor other : visitors) {
+                    if (!visitor.equals(other)) {
+                        try {
+                            visitor.act(currentDate, other);
+                            addEvent(visitor.describe(currentDate, other), currentDate);
+                        } catch (ScenarioException e) {
+                            e.getMessage();
+                        }
+                    }
+                }
+            }
+            System.out.println(describe(currentDate));
+        }
+    }
+
+    @Override
+    public void addEvent(String description, LocalDate date) {
+        events.add(new Event(description, date));
+    }
+
+    public void addVisitor(IslandVisitor visitor) {
+        visitors.add(visitor);
+    }
+
+    @Override
+    public ArrayList<Event> getEventsByDate(LocalDate date) {
+        ArrayList<Event> eventsByDate = new ArrayList<>();
+        for (Event event : events) {
+            if (event.getDate().equals(date) && !event.toString().isEmpty()) {
+                eventsByDate.add(event);
+            }
+        }
+        return eventsByDate;
+    }
+
+    @Override
+    public String toString() {
+        return "остров " + this.getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Island) || this.hashCode() != o.hashCode()) { return false; }
+        Island other = (Island) o;
+        return this.getName().equals(other.getName());
+    }
+
+    @Override
+    public String describe(LocalDate date) {
+        return getEventsByDate(date).toString().replace("[","").replace("]","");
+    }
+}
---
+++ Программирование/Lab4/src/places/Place.java
@@ -0,0 +1,57 @@
+package places;
+import events.Event;
+import exceptions.NameException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public abstract class Place {
+    private final String name;
+    public ArrayList<Event> events = new ArrayList<>();
+
+    protected Place(String name) {
+        this.name = name;
+        if (name.isEmpty())
+            throw new NameException("У места должно быть название");
+    }
+
+    public abstract void startEvents(LocalDate startDate, int eventsDuration);
+
+    public void addEvent(String description, LocalDate date) {
+        events.add(new Event(description, date));
+    }
+
+    public ArrayList<Event> getEventsByDate(LocalDate date) {
+        ArrayList<Event> eventsByDate = new ArrayList<>();
+        for (Event event : events) {
+            if (event.getDate().equals(date) && !event.toString().isEmpty()) {
+                eventsByDate.add(event);
+            }
+        }
+        return eventsByDate;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "место " + this.getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Place) || this.hashCode() != o.hashCode()) { return false; }
+        Place other = (Place) o;
+        return this.getName().equals(other.getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String describe(LocalDate date) {
+        return getEventsByDate(date).toString().replace("[","").replace("]","");
+    }
+}

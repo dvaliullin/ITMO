305c97c5ba1dbb52699c85fa9e6391b1fb5549aa
dvaliullin <dvaliullin266@gmail.com>
2026-02-13 18:17:05 +0300

    Update Sing.java

--- Программирование/Lab2/src/moves/Sing.java
+++ Программирование/Lab2/src/moves/Sing.java
@@ -1,4 +1,4 @@
-package move;
+package moves;
 import ru.ifmo.se.pokemon.*;
 
 
@@ -16,3 +16,4 @@ public class Sing extends StatusMove {
         return "применяет статусную нормальную атаку";
     }
 }
+

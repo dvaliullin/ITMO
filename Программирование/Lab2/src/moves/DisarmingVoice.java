249a311dc5e7a5231a2e25b65caa9c1bc49415ad
dvaliullin <dvaliullin266@gmail.com>
2026-02-14 13:52:48 +0300

    Update DisarmingVoice.java

--- Программирование/Lab2/src/moves/DisarmingVoice.java
+++ Программирование/Lab2/src/moves/DisarmingVoice.java
@@ -6,6 +6,7 @@ public class DisarmingVoice extends SpecialMove {
     public DisarmingVoice() {
         super(Type.FAIRY, 40, Double.POSITIVE_INFINITY);
     }
+
     @Override
     protected String describe() {
         return "применяет специальную феерическую атаку";

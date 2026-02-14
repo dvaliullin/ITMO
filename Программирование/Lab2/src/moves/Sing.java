8850877e2d6419b7cf360ce5fe68ae87589c81b3
dvaliullin <dvaliullin266@gmail.com>
2026-02-14 13:54:49 +0300

    Update Sing.java

--- Программирование/Lab2/src/moves/Sing.java
+++ Программирование/Lab2/src/moves/Sing.java
@@ -6,11 +6,13 @@ public class Sing extends StatusMove {
     public Sing() {
         super(Type.NORMAL,0,55);
     }
+
     @Override
     protected void applyOppEffects(Pokemon p) {
         Effect e = new Effect().condition(Status.SLEEP).turns(3);
         p.addEffect(e);
     }
+
     @Override
     protected String describe() {
         return "применяет статусную нормальную атаку";

d9f394275f93996026b0269b6cde49c8520164ba
dvaliullin <dvaliullin266@gmail.com>
2026-02-15 02:41:11 +0300

    Add files via upload

---
+++ Информатика/Lab4/Dop4/DopTask4.py
@@ -0,0 +1,33 @@
+from MainTask import deserialize
+from DopTask1 import serialize_toml
+from DopTask3 import serialize_xml
+import yaml
+import toml
+import time
+
+with open('расписание.yaml', 'r', encoding='utf-8') as f:
+    yaml_data = f.read()
+
+start_time = time.time()
+for i in range(100):
+    result = deserialize(yaml_data)
+    xml = serialize_xml(result, "расписание")
+xml_time = time.time() - start_time
+
+print('yaml to xml time:',xml_time)
+
+start_time = time.time()
+for i in range(100):
+    result = deserialize(yaml_data)
+    toml_output = serialize_toml(result)
+toml_time_1 = time.time() - start_time
+
+print('yaml to toml time:',toml_time_1)
+
+start_time = time.time()
+for i in range(100):
+    result_yaml = yaml.safe_load(yaml_data)
+    result_toml = toml.dumps(result_yaml)
+toml_time_2 = time.time() - start_time
+
+print('yaml to toml with libs time:',toml_time_2)

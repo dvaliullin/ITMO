9739f09d9e0d8f7d3e171d20a5cfc9b0504d5445
dvaliullin <dvaliullin266@gmail.com>
2026-02-14 13:57:39 +0300

    Update WorkUp.java

--- Программирование/Lab2/src/moves/WorkUp.java
+++ Программирование/Lab2/src/moves/WorkUp.java
@@ -6,10 +6,12 @@ public class WorkUp extends StatusMove {
     public WorkUp() {
         super(Type.NORMAL,0,0);
     }
+
     @Override
     protected void applySelfEffects(Pokemon p) {
         Effect e = new Effect().stat(Stat.ATTACK,1).stat(Stat.SPECIAL_ATTACK,1);
     }
+
     @Override
     protected String describe() {
         return "применяет статусную нормальную атаку";

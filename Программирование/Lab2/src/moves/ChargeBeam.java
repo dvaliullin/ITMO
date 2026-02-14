ed242ebcb0e4c4e261cc22a9abbcdd9007063c27
dvaliullin <dvaliullin266@gmail.com>
2026-02-14 13:51:35 +0300

    Update ChargeBeam.java

--- Программирование/Lab2/src/moves/ChargeBeam.java
+++ Программирование/Lab2/src/moves/ChargeBeam.java
@@ -6,6 +6,7 @@ public class ChargeBeam extends SpecialMove {
     public ChargeBeam() {
         super(Type.ELECTRIC,50,90);
     }
+
     @Override
     protected void applySelfEffects(Pokemon p) {
         if (Math.random() < 0.7) {
@@ -13,6 +14,7 @@ public class ChargeBeam extends SpecialMove {
             p.addEffect(e);
         }
     }
+
     @Override
     protected String describe() {
         return "применяет специальную электрическую атаку";

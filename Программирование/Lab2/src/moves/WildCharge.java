5dd2da456f66da1e543fa28ca3f2bb70bd56c3de
dvaliullin <dvaliullin266@gmail.com>
2026-02-14 13:56:42 +0300

    Update WildCharge.java

--- Программирование/Lab2/src/moves/WildCharge.java
+++ Программирование/Lab2/src/moves/WildCharge.java
@@ -7,10 +7,12 @@ public class WildCharge extends PhysicalMove {
     public WildCharge() {
         super(Type.ELECTRIC, 90, 100);
     }
+
     @Override
     protected void applySelfDamage(Pokemon p, double d) {
         p.setMod(Stat.HP,(int) Math.round(d/4));
     }
+
     @Override
     protected String describe() {
         return "применяет физическую электрическую атаку";

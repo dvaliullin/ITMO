324d3b3956f373001cf3ca9c4226f59fa5676e92
dvaliullin <dvaliullin266@gmail.com>
2026-02-15 03:02:03 +0300

    Update DopTask2.py

--- Информатика/Lab4/Dop2/DopTask2.py
+++ Информатика/Lab4/Dop2/DopTask2.py
@@ -1,13 +1,14 @@
 import yaml
 import toml
 
-with open('расписание.yaml', 'r', encoding='utf-8') as f:
+with open('Data/расписание.yaml', 'r', encoding='utf-8') as f:
     yaml_data = yaml.safe_load(f)
 
-with open('MainTaskResult_2.txt', 'w', encoding='utf-8') as f:
+with open('Data/MainTaskResult_2.txt', 'w', encoding='utf-8') as f:
     f.write(str(yaml_data))
 
-with open('расписание_2.toml', 'w', encoding='utf-8') as f:
+with open('Data/расписание_2.toml', 'w', encoding='utf-8') as f:
     toml.dump(yaml_data, f)
 
 print(yaml_data)
+
